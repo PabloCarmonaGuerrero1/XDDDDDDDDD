df0ae84354042cf97d2def17b2473d8b1db1013f
Pablo <pcargue233@g.educaand.es>
2023-05-15 20:02:28 +0200

    Añadiendo lo de casa

--- src/jvmMain/kotlin/p2.kt
+++ src/jvmMain/kotlin/p2.kt
@@ -1,4 +1,3 @@
-import Proyecto.usuario
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
@@ -10,32 +9,72 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.sql.DriverManager
-
 fun main()  = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
 }
-
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var paginaactual by remember { mutableStateOf("Inicio") }
+    var user by remember { mutableStateOf("Nadie") }
+    when(paginaactual){
+        "Inicio"->MaterialTheme {
+            var usuario by remember { mutableStateOf("") }
+            var contraseña by remember { mutableStateOf("") }
+            var ic by remember { mutableStateOf(false) }
+            var contadorclicks=0
+            var registro by remember { mutableStateOf(false) }
+            MaterialTheme {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Tienda de videojuegos", modifier = Modifier.padding(bottom = 16.dp))
+                    OutlinedTextField(
+                        value = usuario,
+                        onValueChange = { usuario = it },
+                        label = { Text("Nombre de usuario") },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    OutlinedTextField(
+                        value = contraseña,
+                        onValueChange = { contraseña = it },
+                        label = { Text("Contraseña") },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    Button(onClick = { ic = iniciarsesion(usuario,contraseña) ;  contadorclicks++ ; registro=true }) {
+                        Text("Iniciar")
 
-    MaterialTheme {
-        var usuario by remember { mutableStateOf("") }
-        var contraseña by remember { mutableStateOf("") }
-        var ic by remember { mutableStateOf("") }
-        MaterialTheme {
+                    }
+                    if (registro==true){
+                        if (contadorclicks>0 && ic==false) {
+                            Text("ERROR USUARIO Y/O CONTRASEÑA INCORRECTO/S")
+                        }
+                        else if(contadorclicks>0 && ic==true && usuario.isNotBlank() && contraseña.isNotBlank()){
+                            paginaactual="Registrado"
+                            user = usuario
+                        }
+                    }
+                    Button(onClick = { paginaactual="Registrar" }) {
+                        Text("Registrar")
+
+                    }
+                }
+
+            }
+        }
+        "Registrado"->MaterialTheme{
             Column(
                 modifier = Modifier
                     .padding(16.dp)
@@ -43,53 +82,160 @@ fun App() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text("Gestion de Productos", modifier = Modifier.padding(bottom = 16.dp))
-                OutlinedTextField(
-                    value = usuario,
-                    onValueChange = { usuario = it },
-                    label = { Text("Nombre de usuario") },
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                OutlinedTextField(
-                    value = contraseña,
-                    onValueChange = { contraseña = it },
-                    label = { Text("Contraseña") },
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
+                Text("Elige lo que quieres hacer", modifier = Modifier.padding(bottom = 16.dp))
+                Button(onClick = { paginaactual="Juegos" }) {
+                    Text("Ver juegos a la venta")
 
-            }
-            Button(onClick = { ic = iniciarsesion(usuario(usuario,contraseña)).toString() }) {
-                Text("Iniciar")
-            }
-            if (ic.isNotBlank()) {
+                }
+                Button(onClick = { paginaactual="Comprar" }) {
+                    Text("Comprar un videojuego")
 
+                }
+                Button(onClick = { paginaactual="Devolver" }) {
+                    Text("Devolver un videojuego")
+
+                }
+                Button(onClick = { paginaactual="Inicio" }) {
+                    Text("Volver atrás")
+
+                }
             }
+        }
+        "Registrar"-> MaterialTheme {
+            var usuario by remember { mutableStateOf("") }
+            var contraseña by remember { mutableStateOf("") }
+            var repetir by remember { mutableStateOf("") }
+            var registrado by remember { mutableStateOf(false) }
+            var registro by remember { mutableStateOf(false) }
+            MaterialTheme{
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Crea tu usuario!!", modifier = Modifier.padding(bottom = 16.dp))
+                    OutlinedTextField(
+                        value = usuario,
+                        onValueChange = { usuario = it },
+                        label = { Text("Nombre de usuario") },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    OutlinedTextField(
+                        value = contraseña,
+                        onValueChange = { contraseña = it },
+                        label = { Text("Contraseña") },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    OutlinedTextField(
+                        value = repetir,
+                        onValueChange = { repetir = it },
+                        label = { Text("Repetir contraseñna") },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    Button(onClick = { registrado = crearusuario(usuario,contraseña,repetir) ; registro = true }) {
+                        Text("Crear usuario")
+
+                    }
+                    if (registro==true){
+                        if ( registrado==false && usuario.isNotBlank() && contraseña.isNotBlank()) {
+                            Text("ERROR LAS CONTRASEÑAS NO COINCIDEN")
+                        }
+                        else if(registrado==true && usuario.isNotBlank() && contraseña.isNotBlank()){
+                            paginaactual="Inicio"
+                        }
+                    }
+                    Button(onClick = { paginaactual="Inicio" }) {
+                        Text("Volver atrás")
+
+                    }
+                }
+            }
+
 
         }
+        "Juegos"-> MaterialTheme {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                val url = "jdbc:oracle:thin:@localhost:1521:XE"
+                val username = "alumno"
+                val password = "alumno"
+
+                val connection = DriverManager.getConnection(url, username, password)
+
+                val query = "SELECT * FROM Videojuegos"
+
+                val statement = connection.createStatement()
+                val resultSet = statement.executeQuery(query)
+                Text("Nombre de los juegos disponibles")
+                Text("--------------------------------")
+                while (resultSet.next()) {
+                    val nombre = resultSet.getString("titulo")
+                    Text("$nombre")
+                }
+
+                resultSet.close()
+                statement.close()
+                connection.close()
+            }
+            Button(onClick = { paginaactual="Registrado" }) {
+                Text("Volver atrás")
+
+            }
+        }
     }
-
-
 }
-fun iniciarsesion(usuario: usuario){
-    var nombre=usuario.nombre
-    var contraseña = usuario.contrasenya
-    val conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe")
-    val query = "SELECT name, password FROM Usuarios WHERE name = ? and password = ?"
 
+fun iniciarsesion(nombre: String, contrasena: String): Boolean {
+    val url = "jdbc:oracle:thin:@localhost:1521:xe"
+    val usuario = "alumno"
+    val contraseña = "alumno"
+    Class.forName("oracle.jdbc.driver.OracleDriver")
+    var resultado = false
+    val conn = DriverManager.getConnection(url,usuario,contraseña)
+    val query = "SELECT * FROM Usuarios"
     val statement = conn.prepareStatement(query)
-    statement.setString(1, nombre)
-    statement.setString(2, contraseña)
 
     val resultSet = statement.executeQuery()
     while (resultSet.next()) {
-        val foundName = resultSet.getString("name")
-        val password = resultSet.getString("password")
-        println("Name: $foundName, Password: $password")
+        val nom = resultSet.getString("name")
+        val cont= resultSet.getString("password")
+        if (nom==nombre && cont== contrasena) {
+            resultado = true
+        }
     }
-
     resultSet.close()
     statement.close()
     conn.close()
+    return resultado
 }
-
-
+fun crearusuario(nombre: String,contrasena: String,repetir:String):Boolean {
+    var resultado = false
+    val url = "jdbc:oracle:thin:@localhost:1521:xe"
+    val usuario = "alumno"
+    val contraseña = "alumno"
+    Class.forName("oracle.jdbc.driver.OracleDriver")
+    val conexion = DriverManager.getConnection(url, usuario, contraseña)
+    val stmt = conexion.prepareStatement("INSERT INTO Usuarios  (name,password) VALUES (?,?)")
+    if (contrasena==repetir){
+        stmt.setString(1, nombre)
+        stmt.setString(2, contrasena)
+        stmt.executeUpdate()
+        conexion.close()
+        resultado = true
+    }
+    return  resultado
+}
+//CREATE TABLE videojuegos (
+//    titulo VARCHAR2(255),
+//    desarrollador VARCHAR2(255),
+//    plataforma VARCHAR2(255),
+//    fecha_lanzamiento DATE,
+//    precio NUMBER(10,2)
+//);
